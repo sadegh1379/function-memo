88ae5d51cf387756de8df2d18214dbc6142a4074
sadegh <akbarisadegh382@gmail.com>
2024-03-18 01:12:40 -1000

    fix: consol

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ function memoize(fn) {
 
     return function (param) {
         if (cache[param]) {
-            console.log('cached');
             return cache[param];
         } else {
             const result = fn(param);
             cache[param] = result;
-            console.log('not cached');
             return result;
         }
     };
